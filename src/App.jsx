b6a1128985ca1a91021229e04c323ec050530a99
gihantha <gihantha01@gmail.com>
2025-03-12 14:31:09 +0530

    feat(Home):created home screnn with an intro

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import "./index.css";
 import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -19,6 +20,7 @@ function App() {
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   );
